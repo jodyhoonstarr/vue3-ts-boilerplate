ee333b664eaff54fc1333082d10f0928aaadc292
Jody Hoon-Starr <jodyhoonstarr@gmail.com>
2020-12-09 22:10:59 -0500

    end of ch8

--- src/models/api-client/items/ItemsApiClient.model.ts
+++ src/models/api-client/items/ItemsApiClient.model.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosRequestConfig, AxiosError, AxiosResponse } from "axios";
+import { HttpClient, HttpRequestParamsInterface } from "@/models/http-client";
 
 import { ItemsApiClientUrlsInterface } from "@/models/api-client/items/ItemsApiClientUrls.interface";
 import { ItemsApiClientInterface } from "@/models/api-client/items/ItemsApiClient.interface";
@@ -12,22 +12,10 @@ export class ItemsApiClientModel implements ItemsApiClientInterface {
   }
 
   fetchItems(): Promise<ItemInterface[]> {
-    return new Promise<ItemInterface[]>((resolve) => {
-      const url = this.urls.fetchItems;
-
-      // axios options
-      const options: AxiosRequestConfig = {
-        headers: {},
-      };
-
-      axios
-        .get(url, options)
-        .then((response: AxiosResponse) => {
-          resolve(response.data as ItemInterface[]);
-        })
-        .catch((error: any) => {
-          console.error("ItemsApiClient: HttpClient: Get: error", error);
-        });
-    });
+    const getParameters: HttpRequestParamsInterface = {
+      url: this.urls.fetchItems,
+      requiresToken: false,
+    };
+    return HttpClient.get<ItemInterface[]>(getParameters);
   }
 }
